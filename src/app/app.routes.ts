31e7ced8e3642a7e62ba09a1d04a8790c205eba9
alexreino29 <100428734+alexreino29@users.noreply.github.com>
2024-06-20 17:10:58 -0500

    Frontend de una tienda de perfumes en angular 17

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+ import { Routes } from '@angular/router';
+ import { InicioComponent } from './inicio/inicio.component';
+import { PerfumesComponent } from './perfumes/perfumes.component';
+import { Component } from '@angular/core';
+import { InformacionComponent } from './informacion/informacion.component';
 
-export const routes: Routes = [];
+ export const routes: Routes = [
+    {path: '', component: InicioComponent }, 
+    {path: 'perfumes', component: PerfumesComponent},
+    {path: 'informacion', component: InformacionComponent},
+    {path: '', redirectTo: '', pathMatch: 'full'}
+
+];
