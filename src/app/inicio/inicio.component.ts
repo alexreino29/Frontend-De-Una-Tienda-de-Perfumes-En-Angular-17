87b3431efab51030bdfb5efdeb3fda6f603db15e
Alex Reino <alexreino61@gmail.com>
2024-10-31 17:10:10 -0500

    Actualización a las vistas

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {RouterLink, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, CommonModule, RouterLink],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+  menu: string = ''
+
+  Opcion(menu: string){
+    this.menu = menu;
+  }
 
 }
